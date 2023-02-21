aaf4d2cf037990687ae0120115da46feff963b73
maxs <xiaosai.ma@ivydad.com>
2023-02-21 18:54:55 +0800

    single

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = (env) => {
+  return {
+    mode: 'development',
+    output: {
+      filename: 'index.js',
+      path: path.resolve(__dirname, 'dist'),
+      libraryTarget: env.production ? 'system' : '' //打包的模式 system模块
+    },
+    module: {
+      rules: [
+        { test: /\.js$/, use: { loader: 'babel-loader' }, exclude: /node_modules/ }
+      ]
+    },
+    plugins: [
+      !env.production && new HtmlWebpackPlugin({
+        template: './public/index.html'
+      })
+    ].filter(Boolean),
+    externals: env.production ? ['react', 'react-dom'] : []
+  }
+
+}
